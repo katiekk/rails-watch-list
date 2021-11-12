02696cde7e392483fd636c3e6b72d56928450705
Katie Lin <katiekklht@gmail.com>
2021-11-12 19:08:45 +0000

    update app

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -5,11 +5,8 @@ class BookmarksController < ApplicationController
   # end
 
   def create
-    # bookmark_params = retrieve_bookmark_params
-    # bookmark_params['list_id'] = params[:list_id]
     @bookmark = Bookmark.new(bookmark_params)
     @list = List.find(params[:list_id])
-    @bookmark.list = @list
     if @bookmark.save
       redirect_to list_path(@list)
     else
@@ -26,6 +23,6 @@ class BookmarksController < ApplicationController
   private
 
   def bookmark_params
-    params.require(:bookmark).permit(:comment, :movie_id)
+    params.require(:bookmark).permit(:movie_id, :comment).merge(params.permit(:list_id))
   end
 end
